34948c6480a54666f110e23a9c6cfe93cda4e745
csayre <chris.cas2007@gmail.com>
2014-04-09 09:49:40 -0400

    Lab 5 changes

--- Lab5-Example/src/driver.java
+++ Lab5-Example/src/driver.java
@@ -1,36 +1,43 @@
 
 import java.net.*;
+import java.util.Scanner;
 import java.io.*;
 import net.sf.json.*;
 import org.apache.commons.lang.exception.*;
 
 public class driver {
 
-public static void main(String[] args) throws Exception {
-String JSonString = readURL("http://api.geonames.org/weatherIcaoJSON?ICAO=KPHL&formatted=true&username=csayre");
-JSONObject x = JSONObject.fromObject(JSonString);
-JSONObject weatherData =(JSONObject)(x.get("weatherObservation")); 
-System.out.println("Weather data for " + weatherData.get("stationName"));
-System.out.println("Temperature is " + weatherData.get("temperature") + " degrees (Celsius) with " + weatherData.get("clouds"));
+	public static void main(String[] args) throws Exception {
 
-}
+		FileInputStream file = new FileInputStream("/Users/ChrisSayre/Documents/Eclipse/Lab5/res/Airports.txt");
+		Scanner scan = new Scanner(file);
+		while(scan.hasNext())
+		{
+			String name = scan.next();
+			String JSonString = readURL("http://api.geonames.org/weatherIcaoJSON?ICAO="+name+"&formatted=true&username=csayre");
+			JSONObject x = JSONObject.fromObject(JSonString);
+			JSONObject weatherData =(JSONObject)(x.get("weatherObservation")); 
+			System.out.println("Weather data for " + weatherData.get("stationName"));
+			System.out.println("Temperature is " + weatherData.get("temperature") + " degrees (Celsius) with " + weatherData.get("clouds"));
+		}
+	}
 
 
-    public static String readURL(String webservice) throws Exception {
-  URL oracle = new URL(webservice);
-  BufferedReader in = new BufferedReader(
-        new InputStreamReader(
-        oracle.openStream()));
+	public static String readURL(String webservice) throws Exception {
+		URL oracle = new URL(webservice);
+		BufferedReader in = new BufferedReader(
+				new InputStreamReader(
+						oracle.openStream()));
 
-  String inputLine;
-  String result = "";
+		String inputLine;
+		String result = "";
 
-  while ((inputLine = in.readLine()) != null)
-      result = result + inputLine;
+		while ((inputLine = in.readLine()) != null)
+			result = result + inputLine;
 
-  in.close();
-  return result;
-    }
+		in.close();
+		return result;
+	}
 }
 
 
