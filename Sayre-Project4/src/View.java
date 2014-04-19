14eca911d663707e96badec420fb951d177bc274
csayre <chris.cas2007@gmail.com>
2014-04-19 10:04:12 -0400

    p4 update

--- Sayre-Project4/src/View.java
+++ Sayre-Project4/src/View.java
@@ -1,16 +1,13 @@
-import java.awt.Toolkit;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.net.URLConnection;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
+import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 
 /**
  * 
@@ -32,6 +29,13 @@ public class View {
 				JSONObject weatherData =(JSONObject)(x.get("weatherObservation")); 
 	 * 
 	 */
+	
+	
+	/**
+	 * create JSON array
+	 */
+	JsonArray steam;
+	
 	public View() throws Exception
 	{
 		try
@@ -39,11 +43,13 @@ public class View {
 
 			String JSonString = readURL("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=5D0615D35FB669702643632129E0CEC9&steamids=76561198008615109");
 			JSONObject x = JSONObject.fromObject(JSonString);
-			JSONObject id =(JSONObject)(x.get("responce"));
-			//json array then break down w for loop extract data
-			System.out.println(x);
-			System.out.println(id);
-			System.out.println(id.toString());
+			steam = new JsonArray();
+			steam.addAll(x);
+//		
+//			//json array then break down w for loop extract data
+//			System.out.println(x);
+//			System.out.println(id);
+//			System.out.println(id.toString());
 //			System.out.println(x.size());
 //			System.out.println(x.toString());
 //			System.out.println(x);
